5291dd1714c6c4bc0afa191dae47a4931738b7b4
Nayan Kyada <nayan.kayada@gupshup.io>
2022-02-28 08:41:05 +0530

    done

---
+++ 1_Basic.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >= 0.7.0 < 0.9.0;
+
+contract Basic {
+    // global var
+    uint global = 5 ;
+
+    // function function-name(pram-list) scope returns(){
+    // statements }
+    function temp() public view returns(uint){
+        // variables writting here are local to this function
+        uint a = 10; // local variable
+        uint b = 20; // local variable
+        return a + b;
+    }
+
+    function sum(uint a,uint b) public view returns(uint){
+        uint a = 10; // local variable
+        uint b = 20; // local variable
+        return a + b + global; // global is global variable
+    }
+}
