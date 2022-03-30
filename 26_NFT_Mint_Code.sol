67e1777e34d99835748d8aa90370f56e28bb94a5
Nayan Kyada <nayan.kayada@gupshup.io>
2022-03-30 18:26:14 +0530

    NFT Mint addes

--- 26_NFT_Mint_Code.sol
+++ 26_NFT_Mint_Code.sol
@@ -1,30 +1,53 @@
 //Contract based on [https://docs.openzeppelin.com/contracts/3.x/erc721](https://docs.openzeppelin.com/contracts/3.x/erc721)
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract MyNFT is ERC721URIStorage, Ownable {
+contract Skulls is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable{
     using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
+    uint256 public minRate = 0.01 ether;
+    Counters.Counter private _tokenIdCounter;
 
-    constructor() ERC721("MyNFT", "NFT") {}
+    constructor() ERC721("Skulls", "SKUL") {}
 
-    function mintNFT(address recipient, string memory tokenURI) public payable returns (uint256)
-    {
-        require(msg.value >= 0.01 ether);
-        _tokenIds.increment();
-
-        uint256 newItemId = _tokenIds.current();
-        _mint(recipient, newItemId);
-        _setTokenURI(newItemId, tokenURI);
-
-        return newItemId;
+    function safeMint(address to,string memory uri) public payable{
+        require(msg.value >= minRate,"Not Enough Ether Sent");
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
     }
-    function transferBalance() public payable onlyOwner{
-        payable(msg.sender).transfer(address(this).balance);
+    function withdraw() public payable onlyOwner{
+        payable(owner()).transfer(address(this).balance);
+    }
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 }
